39c435e56746fd065680ec5dec5cab69f3a5afd7
Scott Kirkwood <scottkirkwood@google.com>
2013-04-17 22:19:38 -0400

    Split refesh.sh into refesh_all.sh and plugins.sh

--- refesh_all.sh
+++ refesh_all.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 # Pulls latest from all plugins.
-# Updates plugins.txt
 
-plugins_txt="$(pwd -P)/plugins.txt"
-echo 'Plugins Scott uses' > $plugins_txt
 set -e # Fail on error
 for d in ../*
 do
@@ -12,7 +9,6 @@ do
     dirname="${d##.*/}"
     echo "$dirname"
     cd $d
-    echo "$dirname: $(git remote -v | grep fetch | cut -f2 | cut -d' ' -f1)" >> $plugins_txt
     echo "git pull"
     git pull
   fi
