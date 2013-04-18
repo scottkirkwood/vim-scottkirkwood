2c27d8af276a564215d5bf772ae7e257afd68ec8
Scott Kirkwood <scott@forusers.com>
2013-04-17 21:38:04 -0400

    Added refesh which refreshes all plugins.  It also updates plugins.txt

---
+++ refresh.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Pulls latest from all plugins.
+# Updates plugins.txt
+
+plugins_txt="$(pwd -P)/plugins.txt"
+echo 'Plugins Scott uses' > $plugins_txt
+set -e # Fail on error
+for d in ../*
+do
+  if [ -d $d ]
+  then
+    dirname="${d##.*/}"
+    echo "$dirname"
+    echo "$dirname: $(git remote -v | grep fetch | cut -f2 | cut -d' ' -f1)" >> $plugins_txt
+    cd $d
+    echo "git pull"
+    git pull
+  fi
+done
